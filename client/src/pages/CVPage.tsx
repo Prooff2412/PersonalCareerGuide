e7ed767edcd40e48d8d55990793721f07fe500a1
Claus Rasmussen <clausrasmussen0103@gmail.com>
2025-07-31 15:09:56 +0200

    Create CVPage.tsx

---
+++ client/src/pages/CVPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CVBuilder from '../components/CVBuilder';
+export default function CVPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">CV Builder</h1>
+      <CVBuilder />
+    </div>
+  );
+}
