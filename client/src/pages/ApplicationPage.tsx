4cc0f1d452f8b00b45ebe7be0104f93dd90dd980
Claus Rasmussen <clausrasmussen0103@gmail.com>
2025-07-31 15:11:37 +0200

    Create ApplicationPage.tsx

---
+++ client/src/pages/ApplicationPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function ApplicationPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-4">Ansøgning Builder</h1>
+      <p>Denne funktion er under udvikling.</p>
+    </div>
+  );
+}
